78887d7f8e81d9763f04d910dd1df9d37295bf6d
akihito818 <akihito452@gmail.com>
2019-10-14 23:52:09 +0900

    AR　画面遷移

---
+++ AR_test/app/src/main/java/nikkeidp/wings/jp/ar11test/MainActivity.kt
@@ -0,0 +1,20 @@
+package nikkeidp.wings.jp.ar11test
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second)
+
+
+
+
+    }
+}
